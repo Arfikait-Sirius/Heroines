1779882fba7a22e59c2cf4db87e767c421f3f159
Arfikait-Sirius <Arfikait.Sirius@gmail.com>
2020-07-19 18:18:59 +0900

    Create Heroines.

---
+++ dir_Sources/Heroines.js
@@ -0,0 +1,5 @@
+import * as _Jessica from "../dir_Grils/Jessica.js" ;
+import * as _Patricia from "../dir_Grils/Patricia.js" ;
+
+export const Jessica = _Jessica ;
+export const Patricia = _Patricia ;
