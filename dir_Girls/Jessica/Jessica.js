9839017f95bd686ffb581a8c2111c3b1eb08982c
Arfikait-Sirius <arfikait.sirius@gmail.com>
2022-06-05 14:52:29 +0900

    Learn fnCreateTextbox(), fnCreateCheckbox(), fnCreateRadio() and fnCreateButton().

--- dir_Girls/Jessica/Jessica.js
+++ dir_Girls/Jessica/Jessica.js
@@ -1,3 +1,12 @@
+const sfnCreateInputElement = ( type, id = undefined ) => {
+     const element = document.createElement( "input" ) ;
+     
+     element.type = type ;
+     element.id = id ;
+
+     return element ;
+} ;
+
 export const fnGetElementFromId = ( id ) => {
      return document.getElementById( id ) ;
 } ;
@@ -5,16 +14,42 @@ export const fnGetElementFromClass = ( className ) => {
      return document.getElementsByClassName( className ) ;
 } ;
 
+export const fnCreateTextbox = ( id = undefined ) => {
+     return sfnCreateInputElement( "text", id ) ;
+} ;
+export const fnCreateCheckbox = ( id = undefined ) => {
+     return sfnCreateInputElement( "checkbox", id ) ;
+} ;
+export const fnCreateRadio = ( id = undefined ) => {
+     return sfnCreateInputElement( "radio", id ) ;
+} ;
+
+export const fnCreateButton = ( id = undefined ) => {
+     const element = document.createElement( "button" ) ;
+     
+     element.id = id ;
+
+     return element ;
+} ;
+
+export const fnCreateLabel = ( id = undefined ) => {
+     const element = document.createElement( "label" ) ;
+     
+     element.id = id ;
+
+     return element ;
+} ;
+
 export const fnGrantID = ( element, id ) => {
-     element.setAttribute( "id", id ) ;
+     element.id = id ;
 } ;
 
 export const fnGrantClass = ( element, className ) => {
-     element.setAttribute( "class", className ) ;
+     element.className = className ;
 } ;
 
-export const fnGrantNotation = ( element, notation ) => {
-     element.innerHTML = notation ;
+export const fnSetText = ( element, text ) => {
+     element.innerHTML = text ;
 } ;
 
 export const fnAppend = ( parent, child ) => {
@@ -23,4 +58,8 @@ export const fnAppend = ( parent, child ) => {
 
 export const fnAppendNewLine = ( parent ) => {
      parent.appendChild( document.createElement( "br" ) ) ;
+} ;
+
+export const drAddClickEvent = ( element, func ) => {
+     element.onclick = func ;
 } ;
